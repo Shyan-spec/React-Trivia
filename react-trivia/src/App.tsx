fe7bd716eece05de4d01a8f137d75587d21dcd8d
janesfer <sferrazzajane@gmail.com>
2024-02-16 11:04:52 -0500

    Changes on the questions

--- react-trivia/src/App.tsx
+++ react-trivia/src/App.tsx
@@ -1,10 +1,63 @@
+import React, { useState } from "react";
+
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "Boston", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "What year was the Constitution of America written?",
+      options: [
+        { id: 0, text: "1787", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the second president of the US?",
+      options: [
+        { id: 0, text: "John Adams", isCorrect: true },
+        { id: 1, text: "Paul Revere", isCorrect: false },
+        { id: 2, text: "Thomas Jefferson", isCorrect: false },
+        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the US?",
+      options: [
+        { id: 0, text: "California", isCorrect: false },
+        { id: 1, text: "Alaska", isCorrect: true },
+        { id: 2, text: "Texas", isCorrect: false },
+        { id: 3, text: "Montana", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which of the following countries DO NOT border the US?",
+      options: [
+        { id: 0, text: "Canada", isCorrect: false },
+        { id: 1, text: "Russia", isCorrect: true },
+        { id: 2, text: "Cuba", isCorrect: true },
+        { id: 3, text: "Mexico", isCorrect: false },
+      ],
+    },
+  ];
+
   return (
     <>
       <center>
         <h1>USA Quiz</h1>
         <h3>Current Score: 1</h3>
-        <h3> Question 1 out of 5</h3>
+        <h3>
+          Question: {currentQuestion + 1} out of {questions.length}
+        </h3>
         <h3>What year was the Constitution of America written? </h3>
         <ul>
           <li> option 1</li>
